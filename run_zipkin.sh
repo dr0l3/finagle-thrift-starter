f69ad8316b5308bb4edc4b0813bdbfd4e9236b19
Rune Pedersen <rdp@dixa.com>
2018-01-15 15:37:39 +0100

    update zipkin

--- run_zipkin.sh
+++ run_zipkin.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -d -p 9411:9411 -p 9410:9410 -e "SCRIBE_ENABLED=true" -e "JAVA_OPTS=-Dlogging.level.zipkin=DEBUG -Dlogging.level.zipkin2=DEBUG" openzipkin/zipkin
+docker run -d -p 9411:9411 -p 9410:9410 --name zipkin -e "SCRIBE_ENABLED=true" -e "JAVA_OPTS=-Dlogging.level.zipkin=DEBUG -Dlogging.level.zipkin2=DEBUG" openzipkin/zipkin
